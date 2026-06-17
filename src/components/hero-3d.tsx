cd4c04c48e0b88d220c2c2518b47b7b799f92420
Ashu Meena <ashumeena2420@gmail.com>
2026-06-17 18:26:39 +0530

    Remove unsupported config and snap props from PresentationControls

--- src/components/hero-3d.tsx
+++ src/components/hero-3d.tsx
@@ -194,8 +194,6 @@ export function Hero3D() {
         
         <PresentationControls 
           global 
-          config={{ mass: 2, tension: 500 }} 
-          snap={true} 
           rotation={[0, 0, 0]} 
           polar={[-Math.PI / 3, Math.PI / 3]} 
           azimuth={[-Math.PI / 1.4, Math.PI / 2]}

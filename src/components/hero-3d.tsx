4fd5e83a0e581ba67b64bd099135c0459c62a805
Ashu Meena <ashumeena2420@gmail.com>
2026-06-17 17:18:51 +0530

    Fix PresentationControls snap prop type error

--- src/components/hero-3d.tsx
+++ src/components/hero-3d.tsx
@@ -195,7 +195,7 @@ export function Hero3D() {
         <PresentationControls 
           global 
           config={{ mass: 2, tension: 500 }} 
-          snap={{ mass: 4, tension: 1500 }} 
+          snap={true} 
           rotation={[0, 0, 0]} 
           polar={[-Math.PI / 3, Math.PI / 3]} 
           azimuth={[-Math.PI / 1.4, Math.PI / 2]}
